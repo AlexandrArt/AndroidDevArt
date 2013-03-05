d4d5f897a7b7f4f56d68fb58953a1e2729866d8e
Olexandr <fsashaf@bk.ru>
2013-03-05 12:36:05 +0200

    androidDevArt update

--- CustomButtonArts/src/com/android_arts/custombuttonarts/CustomButtonActivity.java
+++ CustomButtonArts/src/com/android_arts/custombuttonarts/CustomButtonActivity.java
@@ -1,22 +1,24 @@
 package com.android_arts.custombuttonarts;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 import android.app.Activity;
-import android.view.Menu;
+
 
 public class CustomButtonActivity extends Activity {
-
+	Button b1;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_custom_button);
+		b1 = (Button) findViewById(R.id.button1);
+		b1.setOnClickListener(new OnClickListener() {
+			   public void onClick(View v) {
+				   Toast.makeText(getApplicationContext(), "button 1 was clicked!", Toast.LENGTH_SHORT).show();      // button 1 was clicked!
+			   }
+			  });		
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_custom_button, menu);
-		return true;
-	}
-
 }
